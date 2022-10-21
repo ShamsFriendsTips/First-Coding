85ab5c0ddb43c3c72d49fda6c60f4f76165afc64
ShamsFriendsTips <116338593+ShamsFriendsTips@users.noreply.github.com>
2022-10-21 18:14:27 +1100

    Add files via upload
    Open this in your coding program

---
+++ Code/main.cpp
@@ -0,0 +1,34 @@
+include <iostream>// header file for imput  and output stream
+
+using namespace std;// adding of namespace (standard namespace of keyword)
+
+int main()
+{
+    // variables
+    //container who contain diferent types f values
+    cout << "Hello world!" << endl;
+    int a= 10;
+    float b=10.987;
+    char i='1';
+    char j='2';
+    string s="hello shams this is tour first c++ program";
+    cout <<"value of a is: "<<a<<endl;
+    cout <<"value of b is: "<<b<<endl;
+    cout <<"value of a+b is: "<<a+b<<endl;
+    cout <<"value of char i= "<<i+j<<endl;
+    cout<<s<<endl;
+    int d;//declaration of variable
+    cout<<"enter the value of int d=";
+    cin>>d;
+    cout<<"output value of int d="<<d<<endl;
+    string name;
+    cout<<"enter your name:";
+    cin>>name;
+    cout<<"your name is: "<<name<<endl;
+    bool var=true;
+    cout<<"bool value is: "<<var;
+
+}
+
+
+//escape sequences in c++
